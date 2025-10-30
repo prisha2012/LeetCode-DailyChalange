65208652bc72f07d6fb8ea29a2b7655bff53ba86
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-10-30 18:39:24 +0530

    3

---
+++ Day3-1526.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int op=target[0];
+        for(int i=1;i<target.length;i++){
+            if(target[i]>target[i-1]){
+                op+=target[i]-target[i-1];
+            }
+        }
+        return op;
+
+     
+    }
+}
+public class Day3-15526 {
+    
+}
