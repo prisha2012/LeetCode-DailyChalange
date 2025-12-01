d44bc8e2e40dc4a2de8658561ccd20e5c040de31
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-12-01 23:53:14 +0530

    35

---
+++ Day35-2141.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for (int b : batteries) sum += b;
+
+        long left = 1, right = sum / n, ans = 0;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (canRun(mid, n, batteries)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean canRun(long T, int n, int[] batteries) {
+        long total = 0;
+        for (int b : batteries) {
+            total += Math.min(b, T);
+        }
+        return total >= (long) n * T;
+    }
+}
+public class Day35-2141 {
+    
+}
