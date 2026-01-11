e221d25b3152e7f50d3c5352f2efa1d66cb37186
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-01-11 22:57:15 +0530

    76

---
+++ Day76.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+          if (matrix.length == 0) return 0;
+          int m = matrix.length, n = matrix[0].length;
+        int[] heights = new int[n];
+        int maxArea = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1'){
+                   heights[j] += 1;
+                }
+                else {
+                    heights[j]=0;
+                }
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+      return maxArea;
+    }
+    private int largestRectangleArea(int [] heights){
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for(int i=0;i<=n;i++){
+        int h=(i==n?0:heights[i]);
+        while(!stack.isEmpty()&&h<heights[stack.peek()]){
+            int height=heights[stack.pop()];
+               int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+        }
+        stack.push(i);
+        }
+        return maxArea;
+    }
+}
+public class Day76 {
+    
+}
